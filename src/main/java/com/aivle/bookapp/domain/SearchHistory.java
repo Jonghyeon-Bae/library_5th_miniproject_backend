f2771fe334d11905ca2ad3efcd91c030e6324f83
JKS <wgj21280@gmail.com>
2026-06-09 14:12:37 +0900

    Entity추가

---
+++ src/main/java/com/aivle/bookapp/domain/SearchHistory.java
@@ -0,0 +1,40 @@
+package com.aivle.bookapp.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="SEARCH_HISTORY")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class SearchHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //외래키 설정 1:M 관계,USER 이름 Not null
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @Setter
+    @Column(nullable = false)
+    private String keyword;
+
+    // 생성될 때 시간 자동 입력  업데이트
+    @Setter
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+}
---
+++ src/main/java/com/aivle/bookapp/domain/Users.java
@@ -0,0 +1,64 @@
+package com.aivle.bookapp.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name="USERS")
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false)
+    private String email;
+
+    @Setter
+    @Column(nullable = false)
+    private String password;
+
+    @Setter
+    @Column(nullable = false)
+    private String name;
+
+
+    @Setter
+    @Column
+    private String avatar;
+
+    @Setter
+    @Column
+    private Boolean email_visibility;
+
+    @Setter
+    @Column
+    private Boolean verified;
+
+    //CreatedDate : 생성될 때 시간 자동 입력  업데이트,
+    @Setter
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+
+    //LastModifiedDate : 수정될 때마다 시간 자동 변경
+    @Setter
+    @LastModifiedDate
+    @Column
+    private LocalDateTime updated_at;
+
+}
