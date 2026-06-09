218e1e55f77cd17a7813a594364066df3979bc9e
Tvle <jonghyeon88@gmail.com>
2026-06-09 17:19:00 +0900

    메인 복구진행 2

--- src/main/java/com/aivle/bookapp/domain/Book.java
+++ src/main/java/com/aivle/bookapp/domain/Book.java
@@ -24,6 +24,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "book")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
--- src/main/java/com/aivle/bookapp/domain/SearchHistory.java
+++ src/main/java/com/aivle/bookapp/domain/SearchHistory.java
@@ -1,45 +1,24 @@
 package com.aivle.bookapp.domain;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-@Entity
-@Getter
-@Setter
-@EntityListeners(AuditingEntityListener.class)
-public class SearchHistory {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    @NotBlank(message = "User ID is required for search history")
-    private Long user_id;
-
-    @Column(nullable = false)
-    private String keyword;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime created_at;
-}
-package com.aivle.bookapp.domain;
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name="SEARCH_HISTORY")
