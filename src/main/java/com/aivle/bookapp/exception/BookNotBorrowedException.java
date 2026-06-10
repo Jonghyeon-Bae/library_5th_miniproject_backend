36a0e404d0fd5ac0bfcaa830577fa69cf4f2c7bc
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2026-06-10 10:50:07 +0900

    exception 내부 변수 이름 변경

--- src/main/java/com/aivle/bookapp/exception/BookNotBorrowedException.java
+++ src/main/java/com/aivle/bookapp/exception/BookNotBorrowedException.java
@@ -4,7 +4,7 @@ package com.aivle.bookapp.exception;
  * 대여 중이 아닌 도서를 반납하려고 할 때 사용한다.
  */
 public class BookNotBorrowedException extends RuntimeException {
-    public BookNotBorrowedException(Long book_id) {
-        super("Book is not borrowed: " + book_id);
+    public BookNotBorrowedException(Long bookId) {
+        super("Book is not borrowed: " + bookId);
     }
 }
--- src/main/java/com/aivle/bookapp/exception/LikeAlreadyExistsException.java
+++ src/main/java/com/aivle/bookapp/exception/LikeAlreadyExistsException.java
@@ -4,7 +4,7 @@ package com.aivle.bookapp.exception;
  * 사용자가 이미 좋아요를 누른 도서에 다시 좋아요를 누르려고 할 때 사용한다.
  */
 public class LikeAlreadyExistsException extends RuntimeException {
-    public LikeAlreadyExistsException(Long book_id, Long user_id) {
-        super("Like already exists: book_id=" + book_id + ", user_id=" + user_id);
+    public LikeAlreadyExistsException(Long bookId, Long userId) {
+        super("Like already exists: bookId=" + bookId + ", userId=" + userId);
     }
 }
