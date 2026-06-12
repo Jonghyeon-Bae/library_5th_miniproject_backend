04ffd9af4e1cd2cc64b2f34de09f42cf5d363e7b
jh <jonghyeon88@gmail.com>
2026-06-12 13:01:15 +0900

    라우팅 러닝 체크

--- src/main/java/com/aivle/bookapp/controller/HelloController.java
+++ src/main/java/com/aivle/bookapp/controller/HelloController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String index() {
+        return "Bookapp Backend is running!";
+    }
+
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok().build();
